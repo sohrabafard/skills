645e0cf19d03d734959f96ede10edb298b4e3ed0
sohrabafard <sohrab.a.fard@gmail.com>
2026-07-10 02:36:34 +0330

    remove redundant skills

--- skills/sohrab/terraform-generator/assets/minimal-project/main.tf
+++
@@ -1,9 +0,0 @@
-# Main Terraform configuration file
-# Add your resources here
-
-# Using terraform_data instead of deprecated null_resource (Terraform 1.4+)
-resource "terraform_data" "example" {
-  triggers_replace = {
-    timestamp = timestamp()
-  }
-}
